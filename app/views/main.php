0cb7675137d34486094105c1371f1914f90c2258
Forge0100 <artem0100@ukr.net>
2018-02-15 22:19:05 +0100

    init class Route add page Login

---
+++ app/views/main.php
@@ -0,0 +1,2 @@
+<h1>Main page</h1>
+<a href="<?=(new App\Services\Route)->link('login')?>">Login</a>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-
-echo "Hello";
+require_once 'app/services/Route.php';
+require_once 'app/index.php';
